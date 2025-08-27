156f29d637e9b8f37ced770ec9deb973e4ab9c0b
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2025-08-27 15:03:15 -0700

    Added animated line

--- src/layouts/Partners.tsx
+++ src/layouts/Partners.tsx
@@ -2,7 +2,51 @@ import { PARTNERS_ITEMS } from "../data/partners";
 import PartnerItem from "../ui/PartnerItem";
 import parse from "html-react-parser";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 function Partners() {
+  useGSAP(() => {
+    const colors = ["rgb(239 246 255)", "rgb(156 163 175)", "rgb(234 179 8)"];
+    gsap.utils
+      .toArray<HTMLElement>(
+        ".animated-partner-subheading, .animated-partner-heading",
+      )
+      .forEach((element) => {
+        const initialColor = element.classList.contains(
+          "animated-partner-heading",
+        )
+          ? colors[0]
+          : colors[1];
+
+        gsap.fromTo(
+          element,
+          { color: initialColor },
+          {
+            scrollTrigger: {
+              trigger: element,
+              start: "top center",
+              end: "bottom center",
+              toggleActions: "play none none reverse",
+              // markers: true,
+              onEnter: () => {
+                gsap.to(element, { color: colors[2] });
+              },
+              onLeave: () => {
+                gsap.to(element, { color: initialColor });
+              },
+              onEnterBack: () => {
+                gsap.to(element, { color: colors[2] });
+              },
+              onLeaveBack: () => {
+                gsap.to(element, { color: initialColor });
+              },
+            },
+          },
+        );
+      });
+  });
+
   return (
     <section className="bg-black py-36">
       <div className="mx-auto grid max-w-[70rem] justify-center gap-16 px-8 sm:grid-cols-2 sm:gap-5">
--- src/ui/PartnerItem.tsx
+++ src/ui/PartnerItem.tsx
@@ -5,13 +5,19 @@ interface PartnerItemProps {
 }
 
 function PartnerItem({ partner }: PartnerItemProps) {
+  const isAnimateItem = partner.name !== "Our Partners";
+
   return (
     <li className="grid grid-cols-[1fr_4fr] gap-3.5">
-      <span className="font-general text-center text-xs font-light text-gray-400 uppercase">
+      <span
+        className={`${isAnimateItem ? "animated-partner-subheading" : "text-grey-500"} font-general text-center text-xs font-light uppercase`}
+      >
         {partner.category}
       </span>
       <div>
-        <h3 className="font-zentry text-2xl tracking-wide text-blue-50 uppercase sm:text-3xl md:text-4xl lg:text-5xl">
+        <h3
+          className={`${isAnimateItem ? "animated-partner-heading" : "text-blue-50"} font-zentry text-2xl tracking-wide text-blue-50 uppercase sm:text-3xl md:text-4xl lg:text-5xl`}
+        >
           {partner.name}
         </h3>
       </div>
