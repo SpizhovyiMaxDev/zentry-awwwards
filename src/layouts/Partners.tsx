4452a54b18a516132fb844fb9e571cc652f06dd9
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2025-08-27 15:15:30 -0700

    Added changing text functionality

--- src/layouts/Partners.tsx
+++ src/layouts/Partners.tsx
@@ -1,11 +1,14 @@
 import { PARTNERS_ITEMS } from "../data/partners";
 import PartnerItem from "../ui/PartnerItem";
 import parse from "html-react-parser";
+import { useState } from "react";
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 function Partners() {
+  const [activePartnerIndex, setActivePartnerIndex] = useState(0);
+
   useGSAP(() => {
     const colors = ["rgb(239 246 255)", "rgb(156 163 175)", "rgb(234 179 8)"];
     gsap.utils
@@ -28,15 +31,29 @@ function Partners() {
               start: "top center",
               end: "bottom center",
               toggleActions: "play none none reverse",
-              // markers: true,
               onEnter: () => {
                 gsap.to(element, { color: colors[2] });
+                const partnerItem = element.closest("li[data-idx]");
+                if (partnerItem) {
+                  const idx = parseInt(
+                    partnerItem.getAttribute("data-idx") || "0",
+                  );
+                  setActivePartnerIndex(idx);
+                }
               },
               onLeave: () => {
                 gsap.to(element, { color: initialColor });
               },
               onEnterBack: () => {
                 gsap.to(element, { color: colors[2] });
+
+                const partnerItem = element.closest("li[data-idx]");
+                if (!partnerItem) return;
+
+                const idx = parseInt(
+                  partnerItem.getAttribute("data-idx") || "0",
+                );
+                setActivePartnerIndex(idx);
               },
               onLeaveBack: () => {
                 gsap.to(element, { color: initialColor });
@@ -52,16 +69,17 @@ function Partners() {
       <div className="mx-auto grid max-w-[70rem] justify-center gap-16 px-8 sm:grid-cols-2 sm:gap-5">
         <div className="max-w-[30rem]">
           <div className="xs:text-md sticky top-1/2 -translate-y-1/2 text-sm text-gray-400 lg:text-lg">
-            {parse(PARTNERS_ITEMS[0].description)}
+            {parse(PARTNERS_ITEMS[activePartnerIndex]?.description || "")}
           </div>
         </div>
         <ul className="max-w-[28rem]">
           <PartnerItem
             partner={{ name: "Our Partners", category: "", description: "" }}
+            index={-1}
             key="Our Partners"
           />
-          {PARTNERS_ITEMS.map((partner) => (
-            <PartnerItem partner={partner} key={partner.name} />
+          {PARTNERS_ITEMS.map((partner, index) => (
+            <PartnerItem partner={partner} key={partner.name} index={index} />
           ))}
         </ul>
       </div>
--- src/ui/PartnerItem.tsx
+++ src/ui/PartnerItem.tsx
@@ -2,21 +2,22 @@ import type { PartnerType } from "../data/partners";
 
 interface PartnerItemProps {
   partner: PartnerType;
+  index?: number;
 }
 
-function PartnerItem({ partner }: PartnerItemProps) {
+function PartnerItem({ partner, index }: PartnerItemProps) {
   const isAnimateItem = partner.name !== "Our Partners";
 
   return (
-    <li className="grid grid-cols-[1fr_4fr] gap-3.5">
+    <li className="grid grid-cols-[1fr_4fr] gap-3.5" data-idx={index}>
       <span
-        className={`${isAnimateItem ? "animated-partner-subheading" : "text-grey-500"} font-general text-center text-xs font-light uppercase`}
+        className={`${isAnimateItem ? "animated-partner-subheading" : ""} font-general text-center text-xs font-light uppercase`}
       >
         {partner.category}
       </span>
       <div>
         <h3
-          className={`${isAnimateItem ? "animated-partner-heading" : "text-blue-50"} font-zentry text-2xl tracking-wide uppercase sm:text-3xl md:text-4xl lg:text-5xl`}
+          className={`${isAnimateItem ? "animated-partner-heading" : ""} font-zentry text-2xl tracking-wide text-blue-50 uppercase sm:text-3xl md:text-4xl lg:text-5xl`}
         >
           {partner.name}
         </h3>
