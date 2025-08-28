e322c218e9b1d74ed648f85ce4ba4dd2638e035a
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2025-08-27 22:07:01 -0700

    Added heading to the footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,5 +1,13 @@
 function Footer() {
-  return <footer className="bg-violet-300"></footer>;
+  return (
+    <footer className="overflow-hidden bg-violet-300 py-4">
+      <div className="mx-auto max-w-[120rem] px-8">
+        <h1 className="font-zentry cursor-pointer text-center text-[20dvw] leading-none tracking-tight text-black uppercase">
+          ZENTRY
+        </h1>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
